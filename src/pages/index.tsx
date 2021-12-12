de9de06935b0983496f4b190167759c0986efba7
Maicon <andrade.maicon.pr@gmail.com>
2021-12-11 22:40:59 -0300

    chore: Starting project with component header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ignews</title>
+      </Head>
+      <h1>Hello <span>Word!</span> </h1>
+    </>
+  )
+}
