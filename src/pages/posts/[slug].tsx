8ff885599af3a8743d7ff9e6ba9b38a34ab0bd42
Maicon <andrade.maicon.pr@gmail.com>
2022-05-01 22:59:28 -0300

    chore: creating post page

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,63 @@
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
+import Head from "next/head"
+import { RichText } from "prismic-dom"
+import { createClient } from "../../services/prismic"
+
+import styles from './post.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  content: string;
+  updateAt: string;
+}
+
+interface PostProps {
+  post: Post;
+}
+
+export default function Post({ post }: PostProps) {
+  return(
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updateAt}</time>
+          <div 
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req, params, previewData}) => {
+  const session = await getSession({ req })
+  const prismic = createClient({ previewData })
+  const {slug} = params;
+
+  const resp = await prismic.getByUID('post', String(slug), {})
+
+  const post = {
+    slug,
+    title: RichText.asText(resp.data.title),
+    content: RichText.asHtml(resp.data.content),
+    updateAt: new Date(resp.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { createClient } from "../../services/prismic";
 import styles from './styles.module.scss';
 import { RichText } from 'prismic-dom';
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -26,11 +27,13 @@ export default function Post({ posts }: PostProps) {
       <div className={styles.posts}>
         {
           posts.map(post => (
-            <a key={post.slug} href="">
-              <time>{post.updateAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))
         }
       </div>
